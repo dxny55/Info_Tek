cc77faccf79ca039d81991895c20174f187849f4
Valeska <valeska_castaneda@ceroca.cat>
2026-03-24 15:58:52 +0100

    3ra columna

--- frontend/src/components/nav/navBar.js
+++
@@ -1,7 +0,0 @@
-export function cargarNav() {
-    fetch("../src/components/nav/navBar.html")
-        .then(res => res.text())
-        .then(html => {
-            document.getElementById("encabezado").innerHTML = html;
-        });
-}
